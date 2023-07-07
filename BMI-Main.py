4f6221230f3b233204d08b1466314315da3fd766
RoyaleYourDad <138877064+RoyaleYourDad@users.noreply.github.com>
2023-07-07 16:27:35 +0500

    BMI
    Code Added You Can Download Code For Free

---
+++ BMI-Main.py
@@ -0,0 +1,10 @@
+#GitHub 1
+
+print("Enter Your Height")
+HeightInput = int(input("Height: "))
+
+print("Enter Your Weight: ")
+WeightInput = int(input("Weight: "))
+
+print("Your BMI: ")
+print(WeightInput / (HeightInput * HeightInput))
